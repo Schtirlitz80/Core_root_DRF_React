81fecb59ed63595ce039a8536ab7e2517bc60c91
Schtirlitz80 <Schtirlitz80@mail.ru>
2023-05-02 10:20:54 +0300

    Initial commit

---
+++ social-media-app/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Home() {
+    return (
+        <div>
+            <h1>Profile</h1>
+            <p>Welcome!</p>
+        </div>
+    );
+}
+
+export default Home; //export отмечает переменные и функции, которые должны быть доступны вне текущего модуля.
